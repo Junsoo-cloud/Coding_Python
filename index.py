13e8b39e1ffdb822f29dc2a155a7d3557a0b6ff6
Junsoo-cloud <kfxsmt1@naver.com>
2022-11-09 11:48:36 +0000

    python

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
-#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
+#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
 print("Hello Python")
 print((3>5)|(5>3))
 print(abs(-5))
 print(3**3)
+# commit 과 push는 앞으로 무조건 터미널에서 실행하는것이 원칙!
+# 36분까지 시청 (유튜브)
